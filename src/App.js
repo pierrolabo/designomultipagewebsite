4d5bfae503b5a4509a07dca0686508fce70bb67e
pierrolabo <pierrolabo@gmail.com>
2021-03-25 15:30:16 +0100

    init project with airbnb styleguide

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The quick brown fox jumps over the lazy dog</h1>
+      <h2>The quick brown fox jumps over the lazy dog</h2>
+      <h3>The quick brown fox jumps over the lazy dog</h3>
     </div>
   );
 }
