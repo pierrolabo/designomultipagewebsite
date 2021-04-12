f9233e6611ffb2c78e7126b48b332c256f9cb905
pierrolabo <pierrolabo@gmail.com>
2021-04-12 22:03:35 +0200

    add router and basic routing pages

---
+++ src/pages/Locations/Locations.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Locations() {
+    return (
+        <section className="locations">
+            <h1>Locations</h1>
+            <h2>Come visit our fancy building</h2>
+        </section>
+    )
+}
