3c92f6facf90be1842c0e87af6f837ac322c3dc7
pierrolabo <pierrolabo@gmail.com>
2021-04-14 23:01:32 +0200

    graphic design ok

--- src/pages/GraphicDesign/GraphicDesign.js
+++ src/pages/GraphicDesign/GraphicDesign.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 import HeaderDesign from "../../components/HeaderDesign/HeaderDesign";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import CardCategory from "../../components/CardCategory/CardCategory";
+
+import BoxedwaterImg from "../../assets/graphic-design/desktop/image-boxed-water.jpg";
+import ChangeImg from "../../assets/graphic-design/desktop/image-change.jpg";
+import ScienceImg from "../../assets/graphic-design/desktop/image-science.jpg";
 
 import './GraphicDesign.scss';
 
@@ -12,6 +18,27 @@ export default function GraphicDesign() {
                 title="Graphic Design"
                 text="We deliver eye-catching branding materials that are tailored to meet your business objectives."
             />
+       <section className="graphicdesign__productlist productlist">
+       <ProductCard
+                    img={ChangeImg}
+                    title="Tim Brown"
+                    text="A book cover designed for Tim Brown’s new release, ‘Change’"
+        />
+        <ProductCard
+                    img={BoxedwaterImg}
+                    title="Boxed water"
+                    text="A simple packaging concept made for Boxed Water"
+        />
+        <ProductCard
+                    img={ScienceImg}
+                    title="Science!"
+                    text="A poster made in collaboration with the Federal Art Project"
+        />
+       </section>
+       <section className="appdesign__category">
+                <CardCategory title="APP DESIGN" link="/app-design"/>
+                <CardCategory title="WEB DESIGN" link="web-design"/>
+            </section>
         </section>
     )
 }
