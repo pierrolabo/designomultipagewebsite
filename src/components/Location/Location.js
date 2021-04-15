ef387eadafbcc30e61ff2139498fe9f3679e9485
pierrolabo <pierrolabo@gmail.com>
2021-04-15 23:18:12 +0200

    location ok

---
+++ src/components/Location/Location.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from "prop-types";
+
+import './Location.scss';
+
+export default function Location({country, office, street, city, phone, mobile, type}) {
+    return (
+        <section className="location">
+            <div className={`location--image ${type}`}/>
+            <div className="location__contents">
+                    <h1 className="location__contents--title">{country}</h1>
+                <div className="location__contents__addresses">
+                    <div className="location__contents__addresses__office">
+                        <h4 className="location__contents__addresses--subtitle">{office}</h4>
+                        <p>{street}</p>
+                        <p>{city}</p>
+                    </div>
+                    <div className="location__contents__addresses__contact">
+                        <h4 className="location__contents__addresses--subtitle">Contact</h4>
+                        <p>{phone}</p>
+                        <p>{mobile}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+Location.propTypes = {
+    country: PropTypes.string.isRequired,
+    office: PropTypes.string.isRequired,
+    street: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    mobile: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+
+}
--- src/pages/Locations/Locations.js
+++ src/pages/Locations/Locations.js
@@ -1,10 +1,39 @@
 import React from 'react'
 
+import Location from "../../components/Location/Location";
+
+import './Locations.scss';
+
 export default function Locations() {
     return (
         <section className="locations">
-            <h1>Locations</h1>
-            <h2>Come visit our fancy building</h2>
+            <Location
+                country="Canada"
+                office="Designo Central Office"
+                street="3886 Wellington Street"
+                city="Toronto, Ontario M9C 3J5"
+                phone="P : +1 253-863-8967"
+                mobile="M : contact@designo.co"
+                type="CANADA"
+            />
+             <Location
+                country="Australia"
+                office="Designo AU Office"
+                street="19 Balonne Street"
+                city="New South Wales 2443"
+                phone="P : (02) 6720 9092"
+                mobile="M : contact@designo.au"
+                type="AUSTRALIA"
+            />
+             <Location
+                country="United Kingdom"
+                office="Designo UK Office"
+                street="13 Colorado Way"
+                city="Rhyd-y-fro SA8 9GA"
+                phone="P : 078 3115 1400"
+                mobile="M : contact@designo.uk"
+                type="UK"
+            />
         </section>
     )
 }
