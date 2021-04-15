69a07455cbf6521bd42008ad8b028ae66ae1cdd7
pierrolabo <pierrolabo@gmail.com>
2021-04-16 00:23:55 +0200

    fix menu elm

--- src/components/Containers/HeaderContainer/HeaderContainer.js
+++ src/components/Containers/HeaderContainer/HeaderContainer.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import {motion} from "framer-motion";
 
 import './HeaderContainer.scss';
@@ -12,14 +12,34 @@ const menuElm = document.getElementById('menu-root');
 
 export default function HeaderContainer() {
     const [isActive, setActive] = useState(false)
+    const history = useHistory();
 
+    // eslint-disable-next-line
+    const handleClick = (e) => {
+        const target = e.target.id
+        switch(target) {
+            case "ABOUT":
+                history.push("/")
+                break;
+                case "LOCATIONS":
+                history.push("/locations")
+                break;
+                case "CONTACT":
+                history.push("/contact")
+                break;
+            default:
+                return null;    
+        }
+        setActive(!isActive)
+
+    }
     function HeaderMenu()  {
            return (
             <motion.div id="header__menu" className="header__menu" animate={{opacity : 1}} transition={{duration: 0.5}}>
             <nav  className="header__menu__nav">
-                    <Link to="/"><h3 className="header__menu__nav--title">OUR COMPANY</h3></Link>
-                    <Link to="/locations"><h3 className="header__menu__nav--title">LOCATIONS</h3></Link>
-                    <Link to="/contact"><h3 className="header__menu__nav--title">CONTACT</h3></Link>
+                    <Link to="/" onClick={(e) => handleClick(e)} ><h3 className="header__menu__nav--title" id="ABOUT">OUR COMPANY</h3></Link>
+                    <Link to="/locations" onClick={(e) => handleClick(e)}><h3 className="header__menu__nav--title" id="LOCATIONS">LOCATIONS</h3></Link>
+                    <Link to="/contact" onClick={(e) => handleClick(e)}><h3 className="header__menu__nav--title" id="CONTACT">CONTACT</h3></Link>
             </nav>
             </motion.div>
            )
@@ -31,9 +51,11 @@ export default function HeaderContainer() {
        }
     }
     useEffect(() => {
-        if(isActive) menuElm.addEventListener('click', handleEscape, false)
+        if(isActive && menuElm) menuElm.addEventListener('click', handleEscape, false)
         return () => {
-            menuElm.removeEventListener('click', handleEscape, false)
+            if(menuElm) {
+                menuElm.removeEventListener('click', handleEscape, false)
+            }
         }
     }, [handleEscape, isActive])
     return (
