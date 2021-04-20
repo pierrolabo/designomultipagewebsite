650c222f343b09bfdc155c8ec137f32b2c656be1
pierrolabo <pierrolabo@gmail.com>
2021-04-20 16:30:11 +0200

    nothing changed on scrolltotop

--- src/hooks/UseScrollToTop.js
+++ src/hooks/UseScrollToTop.js
@@ -7,7 +7,7 @@ export default function UseScrollToTop() {
     useEffect(() => {
         setTimeout(() => {
             // eslint-disable-next-line
-            window.scrollTo(0,0);
+            window.scrollTo(0,0);z
         }, 0)
 
     }, [pathname])
