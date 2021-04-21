16866971e1354efea409dc2066727372a8dd2b91
pierrolabo <pierrolabo@gmail.com>
2021-04-21 15:04:50 +0200

    added locationscard to contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import LocationsCard from '../../components/LocationsCard/LocationsCard';
 
 export default function Contact() {
     return (
         <section className="contact">
             <h1>Hey ! i m Contact page</h1>
             <h2>Contact me now !!!</h2>
+            <LocationsCard/>
         </section>
     )
 }
