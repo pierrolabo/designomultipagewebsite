493a3c749d59c5dca38f0c6d0554aca00591d471
pierrolabo <pierrolabo@gmail.com>
2021-04-28 23:18:29 +0200

    refacto contactform bem

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -10,8 +10,8 @@ export default function ContactForm() {
 
         <section className="contactform">
         <article className="contactform__info">
-            <h1>Contact Us</h1>
-            <p>Ready to take it to the next level? Let’s talk about your project or idea and find out how 
+            <h1 className="contactform__info__title">Contact Us</h1>
+            <p className="contactform__info__text">Ready to take it to the next level? Let’s talk about your project or idea and find out how 
   we can help your business grow. If you are looking for unique digital experiences that’s 
   relatable to your users, drop us a line.</p>
         </article>
@@ -36,18 +36,18 @@ export default function ContactForm() {
              {formik => (
 
           <form onSubmit={formik.handleSubmit} className="contactform__form">
-              <div className="contactform__fields">
+              <div className="contactform__container">
               {/* eslint-disable-next-line */}
-              <label htmlFor="name"><input id="name" className="contactform__form--field" type="text" placeholder="Name" {...formik.getFieldProps('name')} />{formik.touched.name && formik.errors.name ? (<span className="contactform--error">{formik.errors.name}</span>) : null}</label>
+              <label className="contactform__form__label contactform__form__label--border" htmlFor="name"><input id="name" className="contactform__form__field" type="text" placeholder="Name" {...formik.getFieldProps('name')} />{formik.touched.name && formik.errors.name ? (<span className="contactform__field--error">{formik.errors.name}</span>) : null}</label>
               {/* eslint-disable-next-line */}
-              <label htmlFor="email"><input id="email" className="contactform__form--field" type="email" placeholder="Email Address" {...formik.getFieldProps('email')} />{formik.touched.email && formik.errors.email ? (<span className="contactform--error">{formik.errors.email}</span>) : null}</label>
+              <label className="contactform__form__label contactform__form__label--border" htmlFor="email"><input id="email" className="contactform__form__field" type="email" placeholder="Email Address" {...formik.getFieldProps('email')} />{formik.touched.email && formik.errors.email ? (<span className="contactform__field--error">{formik.errors.email}</span>) : null}</label>
               {/* eslint-disable-next-line */}
-              <label htmlFor="phone"><input id="phone" className="contactform__form--field" type="tel" placeholder="Phone" {...formik.getFieldProps('phone')} />{formik.touched.phone && formik.errors.phone ? (<span className="contactform--error">{formik.errors.phone}</span>) : null}</label>
+              <label className="contactform__form__label contactform__form__label--border" htmlFor="phone"><input id="phone" className="contactform__form__field" type="tel" placeholder="Phone" {...formik.getFieldProps('phone')} />{formik.touched.phone && formik.errors.phone ? (<span className="contactform__field--error">{formik.errors.phone}</span>) : null}</label>
               {/* eslint-disable-next-line */}
-              <label htmlFor="message"><textarea id="email" className="contactform__form--field" cols="30" row="2" placeholder="Your Message" {...formik.getFieldProps('message')}></textarea>{formik.touched.message && formik.errors.message ? (<span className="contactform--error">{formik.errors.message}</span>) : null}</label>
+              <label className="contactform__form__label contactform__form__label--border" htmlFor="message"><textarea id="email" className="contactform__form__field contactform__form__field__textarea" cols="30" row="2" placeholder="Your Message" {...formik.getFieldProps('message')}></textarea>{formik.touched.message && formik.errors.message ? (<span className="contactform__field--error">{formik.errors.message}</span>) : null}</label>
               </div>
               {
-                  success !== true ? (<button type="submit">Submit</button>) : <h3 className="contactform--success">Success !!</h3>
+                  success !== true ? (<button className="contactform__form__button" type="submit">Submit</button>) : <h3 className="contactform__form--success">Success !!</h3>
               }
               
           </form>
