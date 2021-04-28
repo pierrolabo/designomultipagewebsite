ad3c41a9cda7c90c8774a0a9039d5a1757f0d2a5
pierrolabo <pierrolabo@gmail.com>
2021-04-28 23:50:20 +0200

    refacto cta bem

--- src/components/Cta/Cta.js
+++ src/components/Cta/Cta.js
@@ -8,14 +8,14 @@ export default function Cta() {
         <div className="cta__container">
             <section className="cta">
                 <div className="cta__details">
-                <h1 className="cta__details--title">Let’s talk about your project</h1>
-                <p className="cta__details--text">
+                <h1 className="cta__details__title">Let’s talk about your project</h1>
+                <p className="cta__details__text">
                 Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.
                 </p>
                 </div>
-                <div className="cta__button">
+                <div className="cta__button__container">
                     <Link to="/contact">
-                        <button type="button" className="cta__button--button">Get in touch</button>
+                        <button type="button" className="cta__button">Get in touch</button>
                     </Link>
                 </div>
             </section>
